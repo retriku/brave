d6323ee5e2c0bedf3b963cacb0c9f2599d060b93
retriku <renars.kudins@gmail.com>
2018-04-03 14:32:31 +0300

    try code from brave clojure. first commit.

---
+++ src/brave/core.clj
@@ -0,0 +1,23 @@
+(ns brave.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(str "asd" 1 2 3)
+
+(defn my-map
+  "implement map with reduce"
+  [f input]
+  (reduce #(conj %1 (f %2)) [] input))
+
+(my-map inc [1 2 3])
+
+(comp inc * 3 4)
+
+(defn f
+  "slow function"
+  [x]
+  (Thread/sleep 1000)
+  )
