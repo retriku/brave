0b7ddfdf7ce0eed6700b3bb4b0bcff2a13642ec7
retriku <renars.kudins@gmail.com>
2018-04-05 22:13:02 +0300

    nth-row function

--- src/generator/gen.clj
+++ src/generator/gen.clj
@@ -2,13 +2,50 @@
 
 (defn- stream
   "generates list of pegs"
-  ([]
-   (stream 0 1))
-  ([sum n]
-   (let [new-sum (+ sum n)]
-     (cons new-sum (lazy-seq (stream new-sum (inc n))))))
+  ([f n]
+   (stream [0 1] f n))
+  ([p f n]
+   (let [new-elem (apply f p)
+         next (n (last p))]
+     (cons new-elem
+           (lazy-seq
+            (stream
+             [new-elem next]
+             f
+             n)
+            )
+           )
+     )
+   )
   )
 
-(def max-per-row stream)
+(def max-per-row  (stream + inc))
 
-(take 5 (max-per-row))
+(take 3 max-per-row)
+
+(take 5 (stream + inc))
+
+(defn nth-row
+  "retuns nth row"
+  [row]
+  (if (= row 1)
+    '(1)
+    (let [m (inc (apply max (take
+                             row
+                             max-per-row)))]
+      (range
+       (- m row)
+       m)
+     )
+    )
+  )
+
+(nth-row 5)
+
+
+(defn matrix
+  "generate matrix for number of rows
+  key: cell number
+  value: is pegged
+  (matrix 3) => {1 false, 2 false, 3 true, 4 false, 5 false, 6 false}"
+  [rows])
